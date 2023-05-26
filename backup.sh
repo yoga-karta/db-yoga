dae013f26ad1346dd56ef8244ab625eed297d41b
Dwiyoga Nurkhoiri Fahmi <34036709+yoga-karta@users.noreply.github.com>
2023-05-26 20:27:35 +0700

    membuuat konfig backup.sh

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# MySQL Configuration
+MYSQL_USER="mysql_username"
+MYSQL_PASSWORD="mysql_password"
+MYSQL_HOST="localhost"
+MYSQL_DATABASE="mysql_database_name"
+MYSQL_BACKUP_DIR="/path/to/mysql/backup/directory"
