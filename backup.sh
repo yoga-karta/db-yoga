ea6f381a47cdc2ce42c114e82fd74186a17a51ac
Dwiyoga Nurkhoiri Fahmi <34036709+yoga-karta@users.noreply.github.com>
2023-05-26 20:29:34 +0700

    konfig utama Mysql backup.sh

--- backup.sh
+++ backup.sh
@@ -6,3 +6,8 @@ MYSQL_PASSWORD="mysql_password"
 MYSQL_HOST="localhost"
 MYSQL_DATABASE="mysql_database_name"
 MYSQL_BACKUP_DIR="/path/to/mysql/backup/directory"
+
+
+# Backup MySQL
+MYSQL_FILENAME="$MYSQL_BACKUP_DIR/$MYSQL_DATABASE-$(date +%Y%m%d%H%M%S).sql"
+mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $MYSQL_FILENAME
