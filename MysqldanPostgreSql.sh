6b74fe9a5860d11282863f63a41e62e91784fe95
Dwiyoga Nurkhoiri Fahmi <34036709+yoga-karta@users.noreply.github.com>
2023-05-29 10:33:31 +0700

    Update MysqldanPostgreSql.sh
    Menambahkan deteksi Db yang terpasang di sistem,
    Merapikan tulisan.

--- MysqldanPostgreSql.sh
+++ MysqldanPostgreSql.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
-# Konfigurasi MySQL
-MYSQL_USER="username"
-MYSQL_PASSWORD="password"
-MYSQL_DATABASE="database_name"
+# Cek apakah MySQL terpasang
+if command -v mysql &> /dev/null; then
+    echo "MySQL terdeteksi."
+    
+    # Konfigurasi MySQL
+    MYSQL_USER="username"
+    MYSQL_PASSWORD="password"
+    MYSQL_DATABASE="database_name"
 
-# Konfigurasi PostgreSQL
-POSTGRES_USER="username"
-POSTGRES_PASSWORD="password"
-POSTGRES_DATABASE="database_name"
+    # Nama file backup dengan tanggal saat ini
+    DATE=$(date +%d-%m-%Y)
+    MYSQL_BACKUP_FILENAME="mysql_backup_$DATE.sql"
+    
+    # Backup MySQL
+    mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $MYSQL_BACKUP_FILENAME
+    
+    # Pindahkan file backup MySQL ke direktori tertentu
+    MYSQL_BACKUP_DIR="/path/to/mysql/backup/directory"
+    mv $MYSQL_BACKUP_FILENAME $MYSQL_BACKUP_DIR
+    
+    echo "Backup MySQL selesai."
+    echo "Proses backup database selesai."
+    exit 0
+fi
 
-# Nama file backup dengan tanggal saat ini
-DATE=$(date +%d-%m-%Y)
-MYSQL_BACKUP_FILENAME="mysql_backup_$DATE.sql"
-POSTGRES_BACKUP_FILENAME="postgres_backup_$DATE.sql"
+# Cek apakah PostgreSQL terpasang
+if command -v psql &> /dev/null; then
+    echo "PostgreSQL terdeteksi."
+    
+    # Konfigurasi PostgreSQL
+    POSTGRES_USER="username"
+    POSTGRES_PASSWORD="password"
+    POSTGRES_DATABASE="database_name"
 
-# Backup MySQL
-mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $MYSQL_BACKUP_FILENAME
+    # Nama file backup dengan tanggal saat ini
+    DATE=$(date +%d-%m-%Y)
+    POSTGRES_BACKUP_FILENAME="postgres_backup_$DATE.sql"
+    
+    # Backup PostgreSQL
+    PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U $POSTGRES_USER -d $POSTGRES_DATABASE > $POSTGRES_BACKUP_FILENAME
+    
+    # Pindahkan file backup PostgreSQL ke direktori tertentu
+    POSTGRES_BACKUP_DIR="/path/to/postgres/backup/directory"
+    mv $POSTGRES_BACKUP_FILENAME $POSTGRES_BACKUP_DIR
+    
+    echo "Backup PostgreSQL selesai."
+    echo "Proses backup database selesai."
+    exit 0
+fi
 
-# Backup PostgreSQL
-PGPASSWORD=$POSTGRES_PASSWORD pg_dump -U $POSTGRES_USER -d $POSTGRES_DATABASE > $POSTGRES_BACKUP_FILENAME
-
-# Pindahkan file backup MySQL ke direktori tertentu
-MYSQL_BACKUP_DIR="/home/user/mysql_backup"
-mv $MYSQL_BACKUP_FILENAME $MYSQL_BACKUP_DIR
-
-# Pindahkan file backup PostgreSQL ke direktori tertentu
-POSTGRES_BACKUP_DIR="/home/user/postgres_backup"
-mv $POSTGRES_BACKUP_FILENAME $POSTGRES_BACKUP_DIR
-
-echo "Backup databases selesai"
-
-
+echo "Tidak ada MySQL atau PostgreSQL terdeteksi."
+echo "Proses backup database selesai."
+exit 1
